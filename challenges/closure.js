ecc7156f889d6a72a119140173db48d1fc6293c8
ErikTrethewey-J <erikenobi@gmail.com>
2020-10-16 10:52:11 -0700

    Turning in Sprint

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,16 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+// It can access it because of how scope works. Internal is withing the scope of the function myFunction cause the closure ends after invoking it on line 15. 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function sumation(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+console.log(sumation(4))
